c5dd39a7a6829e15e50de7862cd68bcf30a90f5d
hyunjung <hyunjung.cho@altiall.com>
2025-05-15 11:20:54 +0900

    크로스오리진 에러 서버단 추가, api테스트 데이터 추가

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/users");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5172;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // 프론트엔드 주소
+    credentials: true, // 쿠키 전송 필요 시 true
+  })
+);
 app.use(bodyParser.json());
 app.use("/users", userRoutes);
 
